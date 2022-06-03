19dd4268fb264310c1e033747e5104b5bd45757e
rahil.k <rahil.k@acsiatech.com>
2022-06-03 19:16:56 +0530

    commit 4

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-<<<<<<< HEAD
 	cout<<"hello C++,Birrfgjgljze";
-=======
-	cout<<"hello C++,Biee";
->>>>>>> 624f465f22651b9c7df4e6676bce8bf9cb94df72
 	return 0;
 }
